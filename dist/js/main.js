105cf79b5e2ad858a233a56c96a762d9c978f822
John Runyard <jrunyard3ft@gmail.com>
2022-11-14 17:49:50 -0600

    Fixed JS bug.

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,77 +3,75 @@ var __webpack_exports__ = {};
 /*!******************************!*\
   !*** ./U-4/src/js/script.js ***!
   \******************************/
-// Find name of page
-function parse_active_page() {
-    let paths = window.location.pathname
-    paths = paths.split("/")
 
-    let name = paths[paths.length - 1]
 
-    var node;
+(function () {
+    // Find name of page
+    function parse_active_page() {
+        let paths = window.location.pathname
 
-    console.log(name);
+        paths = paths.split("/")
+        let name = paths[paths.length - 1]
 
-    switch (name) {
-        case "index.html":
-            node = "nav_box1";
-            break;
-        case "stratton.html":
-            node = "nav_box2";
-            break;
-        case "gh.html":
-            node = "nav_box3";
-            break;
-        case "franklin.html":
-            node = "nav_box4";
-            break;
+        var node;
 
+        switch (name) {
+            case "":
+            case "index.html":
+                node = "nav_box1";
+                break;
+            case "stratton.html":
+                node = "nav_box2";
+                break;
+            case "gh.html":
+                node = "nav_box3";
+                break;
+            case "franklin.html":
+                node = "nav_box4";
+                break;
+
+        }
+        activate_nav_link(node)
     }
-    console.log(node)
-    activate_nav_link(node)
-}
 
-// Make nav link active when on page.
-function activate_nav_link(node) {
-    let active_link = document.querySelector(`body .nav .${node}`)
+    // Make nav link active when on page.
+    function activate_nav_link(node) {
+        let active_link = document.querySelector(`body .nav .${node}`)
+        active_link.classList.add('active');
+    }
 
-    console.log()
-
-    active_link.classList.add('active');
-}
-
-function slicks() {
-    $('.slider').slick({
-        centerMode: true,
-        centerPadding: '60px',
-        slidesToShow: 3,
-        dots: true,
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    arrows: false,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 3
+    function slicks() {
+        $('.slider').slick({
+            centerMode: true,
+            centerPadding: '60px',
+            slidesToShow: 3,
+            dots: true,
+            responsive: [
+                {
+                    breakpoint: 768,
+                    settings: {
+                        arrows: false,
+                        centerMode: true,
+                        centerPadding: '40px',
+                        slidesToShow: 3
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        arrows: false,
+                        centerMode: true,
+                        centerPadding: '40px',
+                        slidesToShow: 1
+                    }
                 }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    arrows: false,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 1
-                }
-            }
-        ]
-    });
-}
+            ]
+        });
+    }
 
-
-parse_active_page();
-slicks();
+    parse_active_page();
+    slicks();
+})();
 /******/ })()
 ;
 //# sourceMappingURL=main.js.map
