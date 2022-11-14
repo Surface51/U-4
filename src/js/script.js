0d3e94ca041be636456c43ba9c1ed9457b6af9f1
John Runyard <jrunyard3ft@gmail.com>
2022-11-13 20:13:06 -0600

    Added bundled script.

---
+++ src/js/script.js
@@ -0,0 +1,71 @@
+// Find name of page
+function parse_active_page() {
+    let paths = window.location.pathname
+    paths = paths.split("/")
+
+    let name = paths[paths.length - 1]
+
+    var node;
+
+    console.log(name);
+
+    switch (name) {
+        case "index.html":
+            node = "nav_box1";
+            break;
+        case "stratton.html":
+            node = "nav_box2";
+            break;
+        case "gh.html":
+            node = "nav_box3";
+            break;
+        case "franklin.html":
+            node = "nav_box4";
+            break;
+
+    }
+    console.log(node)
+    activate_nav_link(node)
+}
+
+// Make nav link active when on page.
+function activate_nav_link(node) {
+    let active_link = document.querySelector(`body .nav .${node}`)
+
+    console.log()
+
+    active_link.classList.add('active');
+}
+
+function slicks() {
+    $('.slider').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+}
+
+
+parse_active_page();
+slicks();
